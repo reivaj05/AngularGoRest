65904b2e4e453adda94fbaeba324f7376ccacb1e
Javier Adriel <reivaj0705@gmail.com>
2016-02-29 16:54:59 -0600

    Create service for handling users requests

---
+++ app/users/services/users.service.js
@@ -0,0 +1,30 @@
+(function () {
+    "use strict";
+    angular
+        .module("app.users.services")
+        .factory("Users", Users);
+
+    Users.$inject = ['$http'];
+    
+    function Users($http) {
+        var Users = {
+            getUsers: getUsers,
+            getUser: getUser,
+            createUser: createUser
+        }; 
+
+        return Users;
+
+        function getUsers() {
+            return $http.get("http://localhost:8080/all/");
+        }
+
+        function getUser(id) {
+            return
+        }
+
+        function createUser(user) {
+            return;
+        }
+    }
+})();
