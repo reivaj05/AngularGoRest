0b5c17ea15311683c0e08b230bac917b956ee6b0
Javier Adriel <reivaj0705@gmail.com>
2016-02-29 21:16:19 -0600

    Indent code to make it look better

--- app/users/controllers/users.controller.js
+++ app/users/controllers/users.controller.js
@@ -5,31 +5,31 @@
         .module("app.users.controllers")
         .controller("UsersController", UsersController);
 
-        UsersController.$inject = ['Users'];
+    UsersController.$inject = ['Users'];
 
-        function UsersController(Users) {
-            var self = this;
+    function UsersController(Users) {
+        var self = this;
 
-            self.users = [];
-            self.getUsers = getUsers;
-            self.init = init;
-            
-            self.init();
+        self.users = [];
+        self.getUsers = getUsers;
+        self.init = init;
+        
+        self.init();
 
-            function init() {
-                self.getUsers();
+        function init() {
+            self.getUsers();
+        }
+
+        function getUsers(){
+            Users.getUsers().then(getUsersSuccessHandler, getUsersErrorHandler);
+
+            function getUsersSuccessHandler(data, status, headers, config) {
+                self.users = data.data;
             }
 
-            function getUsers(){
-                Users.getUsers().then(getUsersSuccessHandler, getUsersErrorHandler);
-
-                function getUsersSuccessHandler(data, status, headers, config) {
-                    self.users = data.data;
-                }
-
-                function getUsersErrorHandler(data, status, headers, config) {
-                    console.error("Error retrieving users");
-                }
+            function getUsersErrorHandler(data, status, headers, config) {
+                console.error("Error retrieving users");
             }
         }
+    }
 })();
